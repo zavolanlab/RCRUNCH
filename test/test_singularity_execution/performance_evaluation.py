0783378e17f4365c539960401396a5acb0f08d01
BIOPZ-Katsantoni Maria <maria.katsantoni@unibas.ch>
2023-01-13 11:58:45 +0100

    feat: addition of testing for single, paired end and TR in addition to GN

--- test/test_singularity_execution/performance_evaluation.py
+++ test/test_singularity_execution/performance_evaluation.py
@@ -12,79 +12,83 @@ def main():
     __doc__ = "Evaluate the run based on expected values"
     outpath = os.path.dirname(os.path.realpath(__file__))
     os.chdir(outpath)
-    a = pd.read_csv(
-        'results/GN/PUM2_K562_ENCSR661ICQ_2/PUM2_K562_ENCSR661ICQ_2_total_peaks.csv',
-        sep='\t', header=0, index_col=None)
-    expected = pd.read_csv(
-        'means_crosslink.tsv',
-        sep='\t', header=0, index_col=None)
-    a['center'] = a['start'] + a['crosslink']
+    for runtype in ['GN', 'TR']:
+        for sample in ["PUM2_K562_ENCSR661ICQ_2_paired_end", "PUM2_K562_ENCSR661ICQ_2_single_end"]:
+            a = pd.read_csv(
+                f'results/{runtype}/{sample}/{sample}_total_peaks.csv',
+                sep='\t', header=0, index_col=None)
+            expected = pd.read_csv(
+                'means_crosslink.tsv',
+                sep='\t', header=0, index_col=None)
+            a['center'] = a['start'] + a['crosslink']
 
-    for index, row in a.iterrows():
-        distance = (
-            row['center'] - expected['crosslink'][expected['strand'] == row['strand']]).abs().min()
-        if distance <= 50:
-            a.loc[index, 'distance'] = distance
-        else:
-            a.loc[index, 'distance'] = np.nan
-    rmsd = np.sqrt(np.sum((a["distance"]**2)) / len(a[~a.distance.isna()]))
-    outlier_percentage = len(a[a.distance.isna()])/len(a) * 100
+            for index, row in a.iterrows():
+                distance = (
+                    row['center'] - expected['crosslink'][expected['strand'] == row['strand']]).abs().min()
+                if distance <= 50:
+                    a.loc[index, 'distance'] = distance
+                else:
+                    a.loc[index, 'distance'] = np.nan
+            if runtype == "GN":
+                rmsd = np.sqrt(np.sum((a["distance"]**2)) / len(a[~a.distance.isna()]))
+                outlier_percentage = len(a[a.distance.isna()])/len(a) * 100
 
 
-    # expected  = pd.read_csv(
-    #     'means_peak_center.tsv',
-    #     sep='\t', header=0, index_col=None)
-    # a['center'] = a['start'] +  a['mi']
+            # expected  = pd.read_csv(
+            #     'means_peak_center.tsv',
+            #     sep='\t', header=0, index_col=None)
+            # a['center'] = a['start'] +  a['mi']
 
-    # for index, row in a.iterrows():
-    #     distance = (row['center'] - expected['peak_center'][expected['strand'] == row['strand']]).abs().min()
-    #     if distance <= 50:
-    #         a.loc[index, 'distance'] = distance
-    #     else:
-    #         a.loc[index, 'distance'] = np.nan
-    # sys.stdout.write(f'rmsd of peak_center as center: {np.sqrt(np.sum((a["distance"]**2)) / len(a[~a.distance.isna()]))}\n'
-    # )
-    # sys.stdout.write(f'percentage of outlier peaks: {len(a[a.distance.isna()])/len(a) * 100} %\n'
-    # )
+            # for index, row in a.iterrows():
+            #     distance = (row['center'] - expected['peak_center'][expected['strand'] == row['strand']]).abs().min()
+            #     if distance <= 50:
+            #         a.loc[index, 'distance'] = distance
+            #     else:
+            #         a.loc[index, 'distance'] = np.nan
+            # sys.stdout.write(f'rmsd of peak_center as center: {np.sqrt(np.sum((a["distance"]**2)) / len(a[~a.distance.isna()]))}\n'
+            # )
+            # sys.stdout.write(f'percentage of outlier peaks: {len(a[a.distance.isna()])/len(a) * 100} %\n'
+            # )
 
-    # add the new test best pwm
-    path = f'results/GN/PUM2_K562_ENCSR661ICQ_2/motif_analysis_final/peak_center/motif_enrichment.tsv'
-    a = pd.read_csv(path, sep='\t', header=0, index_col=0)
-    b = a.loc[a['mean_enrichment'][a['motif'].str.contains('trimmed')].idxmax(), 'motif']
-    run = b.split('_')[-1].split('.')[0]
-    name = path.split('/')[-4]
-    path_new = path.replace(
-        'motif_analysis_final',
-        f'motif_analysis_crossvalidation/{run}').replace('motif_enrichment.tsv', f'wlib/{b}')
-    shutil.copy(path_new, f'wlib/{name}')
+            # add the new test best pwm
+            path = f'results/{runtype}/{sample}/motif_analysis_final/peak_center/motif_enrichment.tsv'
+            a = pd.read_csv(path, sep='\t', header=0, index_col=0)
+            b = a.loc[a['mean_enrichment'][a['motif'].str.contains('trimmed')].idxmax(), 'motif']
+            run = b.split('_')[-1].split('.')[0]
+            name = path.split('/')[-4]
+            path_new = path.replace(
+                'motif_analysis_final',
+                f'motif_analysis_crossvalidation/{run}').replace('motif_enrichment.tsv', f'wlib/{b}')
+            shutil.copy(path_new, f'wlib/PUM2_K562_ENCSR661ICQ_2')
 
-    df = pd.DataFrame()
-    for path1 in glob.glob('wlib/*'):
-        name1 = path1.split('/')[-1]
-        for path2 in glob.glob('wlib/*'):
-            name2 = path2.split('/')[-1]
-            sim = getsimilarity(path1, path2)
-            df.loc[name1, name2] = sim
-    mean_sim = np.mean([df.loc['PUM2_K562_ENCSR661ICQ_2', :].mean(), df.loc[:, 'PUM2_K562_ENCSR661ICQ_2'].mean()])
-    
-    sys.stdout.write(f'rmsd of crosslink centers: {rmsd}\n')
-    sys.stdout.write(f'percentage of outlier peaks: {outlier_percentage} % \n')
-    sys.stdout.write(f'motif similarity: {mean_sim}\n')
-    if rmsd < 1.5:
-        sys.stdout.write('Rmsd is low. Test passed. 1/3\n')
-    else:
-        sys.stdout.write('Rmsd seems to be too high 1/3\n')
-    if outlier_percentage < 5:
-        sys.stdout.write('Few outliers detected. Test passed. 2/3\n')
-    else:
-        sys.stdout.write('Rmsd seems to be too high 2/3\n')
-    if mean_sim > 0.75:
-        sys.stdout.write('Motif similarity is high.Test passed 3/3\n')
-    else:
-        sys.stdout.write('Similarity seems to be low 3/3\n')
+            df = pd.DataFrame()
+            for path1 in glob.glob('wlib/*'):
+                name1 = path1.split('/')[-1]
+                for path2 in glob.glob('wlib/*'):
+                    name2 = path2.split('/')[-1]
+                    sim = getsimilarity(path1, path2)
+                    df.loc[name1, name2] = sim
+            mean_sim = np.mean([df.loc['PUM2_K562_ENCSR661ICQ_2', :].mean(), df.loc[:, 'PUM2_K562_ENCSR661ICQ_2'].mean()])
+            sys.stdout.write(f'{runtype} {sample} evaluation\n') 
+            if runtype == "GN":
+                sys.stdout.write(f'rmsd of crosslink centers: {rmsd}\n')
+                sys.stdout.write(f'percentage of outlier peaks: {outlier_percentage} % \n')
+            
+            sys.stdout.write(f'motif similarity: {mean_sim}\n')
+            if runtype == "GN":
+                if rmsd < 1.5:
+                    sys.stdout.write('Rmsd is low. Peaks are highly overlapping. Test passed. 1/3\n')
+                else:
+                    sys.stdout.write('Rmsd seems to be too high. Peaks are far apart. 1/3\n')
+                if outlier_percentage < 5:
+                    sys.stdout.write('Few outliers detected. Test passed. 2/3\n')
+                else:
+                    sys.stdout.write('Too many peaks that are not found in the test runs 2/3\n')
+            if mean_sim > 0.75:
+                sys.stdout.write('Motif similarity is high.Test passed 3/3\n')
+            else:
+                sys.stdout.write('Motif similarity seems to be low 3/3\n\n')
     return
-
-
 
 def getSimilarityScore(wm1, wm2):
     wm1 = np.array(wm1)
